c03252ddd5d219c236e8fd53478c7d5ef94b27e2
KFerrei <116793771+KFerrei@users.noreply.github.com>
2024-06-21 13:12:02 +0200

    Set Up Project
    Provided content
    New grid class
    Training Loop
    (Question 1-2-3)

---
+++ gym_grid/gym_grid/.ipynb_checkpoints/__init__-checkpoint.py
@@ -0,0 +1,14 @@
+import logging
+from gym.envs.registration import register
+
+logger = logging.getLogger(__name__)
+
+register(
+    id='LinearTrack-v0',
+    entry_point='gym_grid.envs:LinearTrackEnv',
+)
+
+register(
+    id='DeadlyGrid-v0',
+    entry_point='gym_grid.envs:DeadlyGridEnv',
+)
